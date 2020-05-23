e1b9da7f3b150166eeb723ec8635949a4ba8c4ec
Wesarut Khm <anubis.smile@gmail.com>
2020-05-23 16:31:30 +0700

    Improvement - Patch - Add initial data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO res_user (id, name, password, username, create_date, update_date) VALUES (1, 'pandasoft', '$2a$07$n0xcjEzoGGVLx.n2MiY2MOzy77h1JYILmJePnbtZ5XKfKsy4DJndi', 'pandasoftadmin', NOW(), NOW());
